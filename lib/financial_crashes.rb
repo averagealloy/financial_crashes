dca78ee55b3091ced7608ae4bb1c79dca9eb7a8e
Mike McTighe <m.mctighe10@gmail.com>
2019-10-04 22:38:51 +0000

    first commit

---
+++ lib/financial_crashes.rb
@@ -0,0 +1,6 @@
+require "financial_crashes/version"
+
+module FinancialCrashes
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/financial_crashes/version.rb
@@ -0,0 +1,3 @@
+module FinancialCrashes
+  VERSION = "0.1.0"
+end
