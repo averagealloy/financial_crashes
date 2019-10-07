19db4813285f12e47e0d3a94996cc922bc54b089
Mike McTighe <m.mctighe10@gmail.com>
2019-10-07 01:49:48 +0000

    old version that im going back to

--- lib/financial_crashes/cli.rb
+++ lib/financial_crashes/cli.rb
@@ -12,60 +12,39 @@ class CLI
 
     @crashes = Crash.now
     @crashes.each.with_index(1) do |crash, i|
-       puts ""
-       puts "#{i}. #{crash.name}"
+      puts ""
+      puts "#{i}. #{crash.name}"
+      puts ""
+    end
+  end
 
-     end
-   end
-
-  # def menu
-  #   input = nil
-  #   while input != "exit"
-  #     puts "what would you like to learn about today?,to see the crashes again type 'crashes'! or to leave the program type exit: "
-  #
-  #     input = gets.strip.downcase
-  #
-  #     if input.to_i > 0 && input.to_i <= Crash.all.count
-  #       the_crash = Crash.all[input.to_i-1]
-  #       puts ""
-  #       puts "The crash you have selected is #{the_crash.name}. Here is some more info about what you have selected :
-  #   #{the_crash.blurb}"
-  #   puts ""
-  #     elsif input == "crashes"
-  #       #list_crashes
-  #     else
-  #     puts "I can't find that crash. try typing 'crashes' to see the list of crashes again"
-  #     end
-  #   end
-  # end
   def menu
-
     input = nil
+    while input != "exit"
+      puts ""
+      puts "what would you like to learn about today?,to see the crashes again type list! or to leave the program type exit: "
+      puts ""
 
-    while input != 'exit'
-
-      puts "what would you like to learn about today?,to see the crashes again type 'crashes'! or to leave the program type exit:"
-        input = gets.chomp
+      input = gets.chomp
 
       if input.to_i > 0 && input.to_i <= Crash.all.count
+        the_crash = Crash.all[input.to_i-1]
         puts ""
-        puts @crashes[input.to_i -1]
-
-        puts "The crash you have selected is #{@crashes.name}. Here is some more info about what you have selected :
-        #{@crashes.blurb}"
-      elsif input == 'crashes'
-        list_crashes
-      else
-        puts ''
-        puts "I can't find that crash. try typing 'crashes' to see the list of crashes again"
+        puts "The crash you have selected is #{the_crash.name}. Here is some more info about what you have selected :
+        #{the_crash.blurb}"
+        puts ""
+      elsif
+        puts ""
+        puts "I can't find that crash. try typing list to see the list of crashes again"
+        puts ""
+      else input == 'exit'
       end
     end
   end
 
-
   def goodbye
-    puts ""
+    puts ''
     puts "I hope you have learned as much as I did making this project have a good day!"
-    puts ""
+    puts ''
   end
 end
--- lib/financial_crashes/crash.rb
+++ lib/financial_crashes/crash.rb
@@ -1,5 +1,46 @@
 class Crash
+attr_accessor :name, :blurb, :url
+@@all = []
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.now
+    doc = Nokogiri::HTML(open("https://www.thegentlemansjournal.com/five-worst-financial-crashes-time/"))
 
 
+
+     crash_1 = self.new
+      crash_1.name = doc.css("div p strong").map {|title|title.text}[0]
+      crash_1.blurb = doc.css(".o-wrapper.o-wrapper--reading.o-wrapper--right p").map{|blurb| blurb.text}[2]
+     crash_1.url = "https://www.thegentlemansjournal.com/five-worst-financial-crashes-time/"
+
+     crash_2 = self.new
+      crash_2.name = doc.css("div p strong").map {|title|title.text}[1]
+      crash_2.blurb = doc.css(".o-wrapper.o-wrapper--reading.o-wrapper--right p").map{|blurb| blurb.text}[4]
+     crash_2.url = "https://www.thegentlemansjournal.com/five-worst-financial-crashes-time/"
+
+     crash_3 = self.new
+      crash_3.name = doc.css("div p strong").map {|title|title.text}[2]
+      crash_3.blurb = doc.css(".o-wrapper.o-wrapper--reading.o-wrapper--right p").map{|blurb| blurb.text}[6]
+     crash_3.url = "https://www.thegentlemansjournal.com/five-worst-financial-crashes-time/"
+
+
+     crash_4 = self.new
+      crash_4.name = doc.css("div p strong").map {|title|title.text}[3]
+      crash_4.blurb = doc.css(".o-wrapper.o-wrapper--reading.o-wrapper--right p").map{|blurb| blurb.text}[8]
+     crash_4.url = "https://www.thegentlemansjournal.com/five-worst-financial-crashes-time/"
+
+     crash_5 = self.new
+      crash_5.name = doc.css("div p strong").map {|title|title.text}[4]
+      crash_5.blurb = doc.css(".o-wrapper.o-wrapper--reading.o-wrapper--right p").map{|blurb| blurb.text}[10]
+     crash_5.url = "https://www.thegentlemansjournal.com/five-worst-financial-crashes-time/"
+
+    @@all
+  end
 
 end
